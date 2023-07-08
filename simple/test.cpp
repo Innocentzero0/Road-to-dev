3b91e0f16b81e19e5d28c92e7bc6cb7cdf506c5e
Innocentzero0 <dexdevprograming@gmail.com>
2023-07-09 00:56:37 +0100

    multidimensional array

--- simple/test.cpp
+++ simple/test.cpp
@@ -200,12 +200,18 @@ double prices[] = { 45.4, 48.3, 65.1, 3.65, 5.78 };
 
 // function to calculate the total price in a shopping cart
 
+//int main() {
+//    double prices[] = { 6.99, 8.5, 42.9, 105.4, 42, 98.1, 2.39, 1.99, 68.76, 9.99, 33.67, 105, 99, 67, 24.56, 0.2, 59.87, 77.2 };
+//    double total = 0;
+//    for (double price : prices) {
+//        total += price;
+//    }
+//    std::cout << total;
+//    return 0;
+//}
+
+// multidimensional array
+int seats[2][3] = { {1, 2, 3}, {4, 5, 6} };
 int main() {
-    double prices[] = { 6.99, 8.5, 42.9, 105.4, 42, 98.1, 2.39, 1.99, 68.76, 9.99, 33.67, 105, 99, 67, 24.56, 0.2, 59.87, 77.2 };
-    double total = 0;
-    for (double price : prices) {
-        total += price;
-    }
-    std::cout << total;
-    return 0;
+        cout << seats[1][2];
 }
