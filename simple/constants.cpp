7bd0e148b364083e414289f497c7eb362b988ad8
HX <106754422+Innocentzero0@users.noreply.github.com>
2023-07-24 01:31:33 +0100

    Preparing to update gitignore

--- simple/constants.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-	const float avogadros_number = 6.022e23, c = 2.998e8;
-	std::cout << "avogadro's number = " << avogadros_number << '\n';
-	std::cout << "speed of light = " << c << '\n';
-	std::cin.get();
-	return 0;
-}
--- simple/simple.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-	std::cout << "give up";
-	sdd::cin.get();
-}
-
