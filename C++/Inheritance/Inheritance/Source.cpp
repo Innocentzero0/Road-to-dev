c866b3142869b07430f5a9a0e078ba8fe4832761
UnsignedCoder <dexdevprograming@gmail.com>
2023-08-15 19:36:56 +0100

    Inheritance: about protected and private members

--- C++/Inheritance/Inheritance/Source.cpp
+++ C++/Inheritance/Inheritance/Source.cpp
@@ -2,8 +2,8 @@
 
 class Bird {
 public:
-    void birdCanFly(int arg) {
-        bool flight = arg;
+    void birdCanFly(bool flight) {
+        flight;
         if (flight) {
             std::cout << "Bird can fly: " << std::boolalpha << flight << std::endl;
         }
@@ -14,13 +14,34 @@ public:
     }
 };
 
-class Phoenix : public Bird {
+
+class Species {
+public: 
+    void speciesName() {
+        std::string specie;
+        std::cout << "What is the species name? ";
+        std::cin >> specie;
+        std::cout << "bird specie: " << specie <<std::endl;
+    }
+};
+
+/*Protected members can also be accessed by the derived variables in the daughter class below
+but private cannot be accessed
+*/
+
+class Phoenix: public Bird, public Species {
 public:
 };
 
+
+
 int main() {
     Phoenix arden;
-    arden.birdCanFly(0);
+    bool flight;
+    std::cout << "Can this Phoenix fly? (1/0)";
+	std::cin >> flight;
+    arden.birdCanFly(flight);
+    arden.speciesName();
     std::cout << "Press enter to continue..." << std::endl;
     std::cin.get();
     return 0;
